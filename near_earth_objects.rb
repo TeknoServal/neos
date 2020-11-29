902438631be2ca0e7c3a64c707907e186fb95eac
Aidan Murray <aidanhansm@gmail.com>
2020-11-28 20:30:35 -0700

    Refactor

--- near_earth_objects.rb
+++ near_earth_objects.rb
@@ -1,37 +1,50 @@
 require 'faraday'
 require 'figaro'
 require 'pry'
+
 # Load ENV vars via Figaro
-Figaro.application = Figaro::Application.new(environment: 'production', path: File.expand_path('../config/application.yml', __FILE__))
+Figaro.application = Figaro::Application.new(environment: 'production', path: File.expand_path('config/application.yml', __dir__))
 Figaro.load
 
+# Gets data for near earth objects from the NASA api
 class NearEarthObjects
   def self.find_neos_by_date(date)
-    conn = Faraday.new(
-      url: 'https://api.nasa.gov',
-      params: { start_date: date, api_key: ENV['nasa_api_key']}
-    )
+    conn = get_connection(date)
     asteroids_list_data = conn.get('/neo/rest/v1/feed')
 
-    parsed_asteroids_data = JSON.parse(asteroids_list_data.body, symbolize_names: true)[:near_earth_objects][:"#{date}"]
+    parsed_asteroids_data = parse_asteroids_data(asteroids_list_data, date)
 
-    largest_astroid_diameter = parsed_asteroids_data.map do |astroid|
+    {
+      astroid_list: format_data(parsed_asteroids_data),
+      biggest_astroid: largest_diameter(parsed_asteroids_data),
+      total_number_of_astroids: parsed_asteroids_data.count
+    }
+  end
+
+  def self.get_connection(date)
+    Faraday.new(
+      url: 'https://api.nasa.gov',
+      params: { start_date: date, end_date: date, api_key: ENV['nasa_api_key'] }
+    )
+  end
+
+  def self.parse_asteroids_data(data, date)
+    JSON.parse(data.body, symbolize_names: true)[:near_earth_objects][:"#{date}"]
+  end
+
+  def self.largest_diameter(data)
+    data.map do |astroid|
       astroid[:estimated_diameter][:feet][:estimated_diameter_max].to_i
-    end.max { |a,b| a<=> b}
+    end.max
+  end
 
-    total_number_of_astroids = parsed_asteroids_data.count
-    formatted_asteroid_data = parsed_asteroids_data.map do |astroid|
+  def self.format_data(data)
+    data.map do |astroid|
       {
         name: astroid[:name],
         diameter: "#{astroid[:estimated_diameter][:feet][:estimated_diameter_max].to_i} ft",
         miss_distance: "#{astroid[:close_approach_data][0][:miss_distance][:miles].to_i} miles"
       }
     end
-
-    {
-      astroid_list: formatted_asteroid_data,
-      biggest_astroid: largest_astroid_diameter,
-      total_number_of_astroids: total_number_of_astroids
-    }
   end
 end
